800994e123a3612d7c4eb4c15d6dc5c1d7d2d678
SAINATH <sainathmeesala@SAINATHs-MacBook-Pro.local>
2024-12-04 15:09:23 +0530

    donnnnneeee

--- index.js
+++ index.js
@@ -1,16 +1,66 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
 const express = require('express');
 const dotenv = require('dotenv');
-const verifySecret = require('./verifySecret')
-const {prisma} = require('./db/config')
-const shippingRoutes = require('./routes/shippingRoutes')
-
+const verifySecret = require('./verifySecret');
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-app.use('/api/shipping',shippingRoutes);
+const addRecord = async (req , res) => {
+  const {userId,productId,count} = req.body;
+  if(!userId || !productId || !count){
+    return res.status(404).json({error : "All fields required"})
+  }
+  const record = await prisma.shipping.create({
+    data :{
+      userId,productId,count
+    }
+  })
+  return res.status(201).json(record)
+}
+
+const cancleRecord = async (req,res) => {
+  const {shippingId} = req.body;
+  if(!shippingId){
+    return res.status(404).json({error : "Missing shippingId"})
+  }
+  const updatedRecord = await prisma.shipping.update({
+    where :{id : shippingId},
+    data :{status:"cancelled"}
+  })
+  return res.status(200).json(updatedRecord)
+}
+
+const getRecords = async (req, res) => {
+  const { userId } = req.query;
+
+  if (userId) {
+    try {
+      const records = await prisma.shipping.findMany({
+        where: { userId: Number(userId) },
+      });
+      return res.status(200).json(records);
+    } catch (err) {
+      return res.status(500).json({ error: "Error fetching records for userId" });
+    }
+  } else {
+    try {
+      const records = await prisma.shipping.findMany();
+      return res.status(200).json(records);
+    } catch (err) {
+      return res.status(500).json({ error: "Error fetching all records" });
+    }
+  }
+};
+
+app.post('/api/shipping/create' , verifySecret , addRecord);
+app.get('/api/shipping/get' ,verifySecret , getRecords)
+app.get('/api/shipping/get?userId=id',verifySecret ,getRecords)
+app.put('/api/shipping/cancel' , verifySecret , cancleRecord)
 
 // Start the server
 const PORT = process.env.PORT || 3000;
